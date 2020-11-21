61f479f569bfa34f2a4893987c665e409901e8ca
va1entei <va1entei@tut.by>
2020-11-21 20:11:50 +0000

    add board

--- tele.py
+++ tele.py
@@ -5,13 +5,24 @@ import telebot
 import time
 #pip3 install pytelegrambotapi --upgrade
 
+board = list(range(1,10))
+
+
+
 bot = telebot.TeleBot('1269788895:AAG3pFUfu4MIFr86iDI9K6xQAKbn1ozJ914')
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     print(message.text)
     if message.text == "/help":
-        bot.send_message(message.from_user.id, "i am stupit")
+        bot.send_message(message.from_user.id, "hello my dear  hoziain im a you slav")
+    elif message.text == "tt":
+        board = list(range(1,10))
+        bot.send_message(message.from_user.id, "-" * 13)
+        for i in range(3):
+            bot.send_message(message.from_user.id,"|"+ str(board[0+i*3])+ "|"+ str(board[1+i*3])+ "|"+ str(board[2+i*3])+ "|")
+            bot.send_message(message.from_user.id, "-" * 13)           
+
     else:
         bot.send_message(message.from_user.id, "i undestend write /help.")
 
